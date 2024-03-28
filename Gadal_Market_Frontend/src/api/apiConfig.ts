b87d3c81b6ed1aaca975a85e7174252467b38229
atse <atseneha@gmail.com>
2024-03-28 11:01:00 +0300

    change api.config

--- Gadal_Market_Frontend/src/api/apiConfig.ts
+++ Gadal_Market_Frontend/src/api/apiConfig.ts
@@ -1,4 +1,6 @@
-const BASE_URL = `http://127.0.0.1:7000/api/`
-export const IMAGE_URL = 'http://127.0.0.1:7000/images'
+// const BASE_URL = `http://127.0.0.1:7000/api/`
+// export const IMAGE_URL = 'http://127.0.0.1:7000/images'
+// export default BASE_URL
+const BASE_URL = `http://159.69.192.236/api/`
+export const IMAGE_URL = 'http://159.69.192.236/images'
 export default BASE_URL
-
